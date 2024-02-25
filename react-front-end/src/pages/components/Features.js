8138b83902e3722012f18d403abd592d4b1978ef
Zhuoy Wang <zhuoyiwang@mines.edu>
2024-02-24 18:31:21 -0700

    Added background color

--- react-front-end/src/pages/components/Features.js
+++ react-front-end/src/pages/components/Features.js
@@ -152,225 +152,235 @@ export default function Features() {
   const selectedFeature = items[selectedItemIndex];
 
   return (
-    <Container id="features" sx={{ py: { xs: 8, sm: 16 } }}>
-      <Grid container spacing={6}>
-        <Grid item xs={12} md={6}>
-          <div>
-            <Typography component="h2" variant="h4" color="text.primary">
-              
-            </Typography>
-            <Typography
-              variant="body1"
-              color="text.secondary"
-              sx={{ mb: { xs: 2, sm: 4 } }}
-            >
-              Click on each box to see more about the topic!
-            </Typography>
-          </div>
-          <Grid container item gap={1} sx={{ display: { xs: 'auto', sm: 'none' } }}>
-            {items.map(({ title }, index) => (
-              <Chip
-                key={index}
-                label={title}
-                onClick={() => handleItemClick(index)}
-                sx={{
-                  borderColor: (theme) => {
-                    if (theme.palette.mode === 'light') {
+    <Box sx={(theme) => ({
+      width: '100%',
+      backgroundImage:
+        theme.palette.mode === 'light'
+          ? 'linear-gradient(180deg, #CEE5FD, #FFF)'
+          : 'linear-gradient(#02294F, #090E10)',
+      backgroundSize: '100% 20%',
+      backgroundRepeat: 'no-repeat',
+    })}>
+      <Container id="features" sx={{ py: { xs: 8, sm: 16 } }}>
+        <Grid container spacing={6}>
+          <Grid item xs={12} md={6}>
+            <div>
+              <Typography component="h2" variant="h4" color="text.primary">
+                
+              </Typography>
+              <Typography
+                variant="body1"
+                color="text.secondary"
+                sx={{ mb: { xs: 2, sm: 4 } }}
+              >
+                Click on each box to see more about the topic!
+              </Typography>
+            </div>
+            <Grid container item gap={1} sx={{ display: { xs: 'auto', sm: 'none' } }}>
+              {items.map(({ title }, index) => (
+                <Chip
+                  key={index}
+                  label={title}
+                  onClick={() => handleItemClick(index)}
+                  sx={{
+                    borderColor: (theme) => {
+                      if (theme.palette.mode === 'light') {
+                        return selectedItemIndex === index ? 'primary.light' : '';
+                      }
                       return selectedItemIndex === index ? 'primary.light' : '';
-                    }
-                    return selectedItemIndex === index ? 'primary.light' : '';
-                  },
-                  background: (theme) => {
-                    if (theme.palette.mode === 'light') {
+                    },
+                    background: (theme) => {
+                      if (theme.palette.mode === 'light') {
+                        return selectedItemIndex === index ? 'none' : '';
+                      }
                       return selectedItemIndex === index ? 'none' : '';
-                    }
-                    return selectedItemIndex === index ? 'none' : '';
-                  },
-                  backgroundColor: selectedItemIndex === index ? 'primary.main' : '',
-                  '& .MuiChip-label': {
-                    color: selectedItemIndex === index ? '#fff' : '',
-                  },
+                    },
+                    backgroundColor: selectedItemIndex === index ? 'primary.main' : '',
+                    '& .MuiChip-label': {
+                      color: selectedItemIndex === index ? '#fff' : '',
+                    },
+                  }}
+                />
+              ))}
+            </Grid>
+            <Box
+              component={Card}
+              variant="outlined"
+              sx={{
+                display: { xs: 'auto', sm: 'none' },
+                mt: 4,
+              }}
+            >
+              <Box
+                sx={{
+                  backgroundImage: (theme) =>
+                    theme.palette.mode === 'light'
+                      ? items[selectedItemIndex].imageLight
+                      : items[selectedItemIndex].imageDark,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  minHeight: 280,
                 }}
               />
-            ))}
-          </Grid>
-          <Box
-            component={Card}
-            variant="outlined"
-            sx={{
-              display: { xs: 'auto', sm: 'none' },
-              mt: 4,
-            }}
-          >
-            <Box
-              sx={{
-                backgroundImage: (theme) =>
-                  theme.palette.mode === 'light'
-                    ? items[selectedItemIndex].imageLight
-                    : items[selectedItemIndex].imageDark,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                minHeight: 280,
-              }}
-            />
-            <Box sx={{ px: 2, pb: 2 }}>
-              <Typography color="text.primary" variant="body2" fontWeight="bold">
-                {selectedFeature.title}
-              </Typography>
-              <Typography color="text.secondary" variant="body2" sx={{ my: 0.5 }}>
-                {selectedFeature.description}
-              </Typography>
-              <Link
-                color="primary"
-                variant="body2"
-                fontWeight="bold"
-                sx={{
-                  display: 'inline-flex',
-                  alignItems: 'center',
-                  '& > svg': { transition: '0.2s' },
-                  '&:hover > svg': { transform: 'translateX(2px)' },
-                }}
-              >
-                <span></span>
-                <ChevronRightRoundedIcon
-                  fontSize="small"
-                  sx={{ mt: '1px', ml: '2px' }}
-                />
-              </Link>
-            </Box>
-          </Box>
-          <Stack
-            direction="column"
-            justifyContent="center"
-            alignItems="flex-start"
-            spacing={2}
-            useFlexGap
-            sx={{ width: '100%', display: { xs: 'none', sm: 'flex' } }}
-          >
-            {items.map(({ icon, title, description }, index) => (
-              <Card
-                key={index}
-                component={Button}
-                onClick={() => handleItemClick(index)}
-                sx={{
-                  p: 3,
-                  height: 'fit-content',
-                  width: '100%',
-                  background: 'none',
-                  backgroundColor:
-                    selectedItemIndex === index ? 'action.selected' : undefined,
-                  borderColor: (theme) => {
-                    if (theme.palette.mode === 'light') {
-                      return selectedItemIndex === index
-                        ? 'primary.light'
-                        : 'grey.200';
-                    }
-                    return selectedItemIndex === index ? 'primary.dark' : 'grey.800';
-                  },
-                }}
-              >
-                <Box
+              <Box sx={{ px: 2, pb: 2 }}>
+                <Typography color="text.primary" variant="body2" fontWeight="bold">
+                  {selectedFeature.title}
+                </Typography>
+                <Typography color="text.secondary" variant="body2" sx={{ my: 0.5 }}>
+                  {selectedFeature.description}
+                </Typography>
+                <Link
+                  color="primary"
+                  variant="body2"
+                  fontWeight="bold"
                   sx={{
+                    display: 'inline-flex',
+                    alignItems: 'center',
+                    '& > svg': { transition: '0.2s' },
+                    '&:hover > svg': { transform: 'translateX(2px)' },
+                  }}
+                >
+                  <span></span>
+                  <ChevronRightRoundedIcon
+                    fontSize="small"
+                    sx={{ mt: '1px', ml: '2px' }}
+                  />
+                </Link>
+              </Box>
+            </Box>
+            <Stack
+              direction="column"
+              justifyContent="center"
+              alignItems="flex-start"
+              spacing={2}
+              useFlexGap
+              sx={{ width: '100%', display: { xs: 'none', sm: 'flex' } }}
+            >
+              {items.map(({ icon, title, description }, index) => (
+                <Card
+                  key={index}
+                  component={Button}
+                  onClick={() => handleItemClick(index)}
+                  sx={{
+                    p: 3,
+                    height: 'fit-content',
                     width: '100%',
-                    display: 'flex',
-                    textAlign: 'left',
-                    flexDirection: { xs: 'column', md: 'row' },
-                    alignItems: { md: 'center' },
-                    gap: 2.5,
+                    background: 'none',
+                    backgroundColor:
+                      selectedItemIndex === index ? 'action.selected' : undefined,
+                    borderColor: (theme) => {
+                      if (theme.palette.mode === 'light') {
+                        return selectedItemIndex === index
+                          ? 'primary.light'
+                          : 'grey.200';
+                      }
+                      return selectedItemIndex === index ? 'primary.dark' : 'grey.800';
+                    },
                   }}
                 >
                   <Box
                     sx={{
-                      color: (theme) => {
-                        if (theme.palette.mode === 'light') {
-                          return selectedItemIndex === index
-                            ? 'primary.main'
-                            : 'grey.300';
-                        }
-                        return selectedItemIndex === index
-                          ? 'primary.main'
-                          : 'grey.700';
-                      },
+                      width: '100%',
+                      display: 'flex',
+                      textAlign: 'left',
+                      flexDirection: { xs: 'column', md: 'row' },
+                      alignItems: { md: 'center' },
+                      gap: 2.5,
                     }}
                   >
-                    {icon}
-                  </Box>
-                  <div>
-                    <Typography
-                      color="text.primary"
-                      variant="body2"
-                      fontWeight="bold"
-                    >
-                      {title}
-                    </Typography>
-                    <Typography
-                      color="text.secondary"
-                      variant="body2"
-                      sx={{ my: 0.5 }}
-                    >
-                      {description}
-                    </Typography>
-                    <Link
-                      color="primary"
-                      variant="body2"
-                      fontWeight="bold"
+                    <Box
                       sx={{
-                        display: 'inline-flex',
-                        alignItems: 'center',
-                        '& > svg': { transition: '0.2s' },
-                        '&:hover > svg': { transform: 'translateX(2px)' },
-                      }}
-                      onClick={(event) => {
-                        event.stopPropagation();
+                        color: (theme) => {
+                          if (theme.palette.mode === 'light') {
+                            return selectedItemIndex === index
+                              ? 'primary.main'
+                              : 'grey.300';
+                          }
+                          return selectedItemIndex === index
+                            ? 'primary.main'
+                            : 'grey.700';
+                        },
                       }}
                     >
-                      <span></span>
-                      <ChevronRightRoundedIcon
-                        fontSize="small"
-                        sx={{ mt: '1px', ml: '2px' }}
-                      />
-                    </Link>
-                  </div>
-                </Box>
-              </Card>
-            ))}
-          </Stack>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={6}
-          sx={{ display: { xs: 'none', sm: 'flex' }, width: '100%' }}
-        >
-          <Card
-            variant="outlined"
-            sx={{
-              height: '100%',
-              width: '100%',
-              display: { xs: 'none', sm: 'flex' },
-              pointerEvents: 'none',
-            }}
+                      {icon}
+                    </Box>
+                    <div>
+                      <Typography
+                        color="text.primary"
+                        variant="body2"
+                        fontWeight="bold"
+                      >
+                        {title}
+                      </Typography>
+                      <Typography
+                        color="text.secondary"
+                        variant="body2"
+                        sx={{ my: 0.5 }}
+                      >
+                        {description}
+                      </Typography>
+                      <Link
+                        color="primary"
+                        variant="body2"
+                        fontWeight="bold"
+                        sx={{
+                          display: 'inline-flex',
+                          alignItems: 'center',
+                          '& > svg': { transition: '0.2s' },
+                          '&:hover > svg': { transform: 'translateX(2px)' },
+                        }}
+                        onClick={(event) => {
+                          event.stopPropagation();
+                        }}
+                      >
+                        <span></span>
+                        <ChevronRightRoundedIcon
+                          fontSize="small"
+                          sx={{ mt: '1px', ml: '2px' }}
+                        />
+                      </Link>
+                    </div>
+                  </Box>
+                </Card>
+              ))}
+            </Stack>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{ display: { xs: 'none', sm: 'flex' }, width: '100%' }}
           >
-            <Box
+            <Card
+              variant="outlined"
               sx={{
-                m: 'auto',
-                width: 420,
-                height: 500,
-                backgroundSize: 'contain',
-                backgroundImage: (theme) =>
-                  theme.palette.mode === 'light'
-                    ? items[selectedItemIndex].imageLight
-                    : items[selectedItemIndex].imageDark,
+                height: '100%',
+                width: '100%',
+                display: { xs: 'none', sm: 'flex' },
+                pointerEvents: 'none',
               }}
-            />
-            <Box sx={{ p: 2 }}>
-              <Typography variant="body1"  sx={{ textAlign: 'center' }}>
-                {selectedFeature.moreText}
-              </Typography>
-            </Box>
-          </Card>
+            >
+              <Box
+                sx={{
+                  m: 'auto',
+                  width: 420,
+                  height: 500,
+                  backgroundSize: 'contain',
+                  backgroundImage: (theme) =>
+                    theme.palette.mode === 'light'
+                      ? items[selectedItemIndex].imageLight
+                      : items[selectedItemIndex].imageDark,
+                }}
+              />
+              <Box sx={{ p: 2 }}>
+                <Typography variant="body1"  sx={{ textAlign: 'center' }}>
+                  {selectedFeature.moreText}
+                </Typography>
+              </Box>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </Box>
   );
 }
